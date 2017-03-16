67aad3a61bda6414d9c10b87be3e0347c7ff8749
Hackbright Student <no-reply@hackbrightacademy.com>
2017-03-15 17:05:14 -0700

    working prototype

---
+++ script.js
@@ -0,0 +1,32 @@
+var WORDS = ["booger", "apple", "hackbright", "mango"];
+var SCRAMBLED = ["oogreb", "papel", "backrhight", "goman"];
+
+var playing = true;
+
+while (playing) {
+
+    // Grab a random word from the WORDS array.
+    var randomIndex = Math.floor(Math.random() * WORDS.length);
+    var realWord = WORDS[randomIndex]
+    // Grab the corresponding scrambled word from the SCRAMBLED array.
+    var scambledWord = SCRAMBLED[randomIndex];
+    // console.log(scambledWord)
+
+    // Prompt the user to guess the original word when given the scrambled word.
+    var guess = prompt("What is this scrambled word?\n" + scambledWord).toLowerCase().replace(/\s/g, '');
+
+    // If the user guesses wrong, tell them they were wrong and show the right word.
+    if (guess === realWord){
+        alert("Good guess!");
+    } else {
+        alert("Wrong, you failure! The word was " + realWord);
+    }
+    // If the user guesses the correct word, congratulate them.
+
+    playing = confirm("Press OK To keep playing or Cancel to quit");
+
+    // What does confirm() return?
+    // What does setting the playing variable do?
+}
+
+alert("goodbye!");
