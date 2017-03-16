6f01f5e75cb204a39856f93b3e411f0063b7daf7
Hackbright Student <no-reply@hackbrightacademy.com>
2017-03-15 17:17:07 -0700

    Self scrambling

--- script.js
+++ script.js
@@ -8,12 +8,23 @@ while (playing) {
     // Grab a random word from the WORDS array.
     var randomIndex = Math.floor(Math.random() * WORDS.length);
     var realWord = WORDS[randomIndex]
-    // Grab the corresponding scrambled word from the SCRAMBLED array.
-    var scambledWord = SCRAMBLED[randomIndex];
+    // // Grab the corresponding scrambled word from the SCRAMBLED array.
+    // var scambledWord = SCRAMBLED[randomIndex];
     // console.log(scambledWord)
 
+    var splitWord = realWord.split("");
+
+    for (var i=0; i<splitWord.length; i++) {
+        var letter = splitWord.pop();
+        splitWord.splice(Math.floor(Math.random() * splitWord.length), 0, letter);
+        console.log(splitWord);
+    }
+
+    var scambledWord = splitWord.join("");
+
     // Prompt the user to guess the original word when given the scrambled word.
-    var guess = prompt("What is this scrambled word?\n" + scambledWord).toLowerCase().replace(/\s/g, '');
+    var guess = prompt("What is this scrambled word?\n"
+                       + scambledWord).toLowerCase().replace(/\s/g, '');
 
     // If the user guesses wrong, tell them they were wrong and show the right word.
     if (guess === realWord){
